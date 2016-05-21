2f016e4582ba26e5ca95e025f9d18f196f5ff601
Kate Voitiuk <kvoitiuk@users.noreply.github.com>
2016-05-21 03:32:58 -0700

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -14,26 +14,26 @@ const int N = 40;
 
 //helper function to sum all elements in some vector d 
 inline void sum(int*p, int n, vector<int> &d) {
-*p = 0;
-for(int i = 0; i < n; ++i) 
-       *p = *p + d.at(i); 
+    *p = 0;
+    for(int i = 0; i < n; ++i) 
+        *p = *p + d.at(i); 
 }
 
 //sums the numbers 1 through N and outputs the result
 int main() {
-int accum = 0;
-vector<int> data(N);
+    int accum = 0;
+    vector<int> data(N);
 
-//populate our vector 
-for(int i = 0; i < N; ++i)
+    //populate our vector 
+    for(int i = 0; i < N; ++i)
         data.at(i)= i;
         
-//perform the summation
-sum(&accum, N, data);
+    //perform the summation
+    sum(&accum, N, data);
 
-//output result to screen
-cout << "sum is " << accum;
+    //output result to screen
+    cout << "sum is " << accum;
 
-return 0;
+    return 0;
 
 }
